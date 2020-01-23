a756c2d8fdb8799dd972bb2f1d3acc1bb528f8e1
Stacy Kim <spk004@ucsd.edu>
2020-01-23 10:03:25 -0800

    Added and changed CSS classes

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -3,8 +3,19 @@
  * GET home page.
  */
 
-exports.view = function(req, res){
+/* exports.view = function(req, res){
   res.render('index', {
   	'name': '',
   });
+}; */
+
+exports.view = function(req, res){
+	var nameToShow = req.params.userName;
+	if (nameToShow === undefined) {
+		nameToShow = 'World';
+	}
+	console.log("Name is " + nameToShow);
+	res.render('index', {
+		'name' : nameToShow,
+	});
 };
